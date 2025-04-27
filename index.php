48afc1ebbbbe99c588215a7df503c264bf0625f3
Ptichka222 <jutube760@gmail.com>
2025-04-27 11:49:48 +0500

    test

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $message = $update["message"]["text"];
 
 // A simple response
 if ($message == "1"){ $response = "2";}
-else {$response = "You said: 2";}
+else {$response = "You said: $message";}
 
 // Send the response back using Telegram API
 file_get_contents("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=" . urlencode($response));
