142d1da5fe9f7667da8b9a60174dd13721ef4f6b
Ptichka222 <jutube760@gmail.com>
2025-04-27 17:02:40 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 # Hugging Face and Bot tokens
-$modeUrl = "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct";  // Update this if necessary
+$modeUrl = "https://huggingface.co/tiiuae/falcon-7b-instruct";  // Update this if necessary
 $hugFace = "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"; // (Use your Hugging Face token here)
 $botToken = "8186610571:AAGQuFiDmn3j21ntnecn_Bd9HMlay46J04A";
 
